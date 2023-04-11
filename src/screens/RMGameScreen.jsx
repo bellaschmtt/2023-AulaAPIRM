e26c24ed3c013e89bfeaed5a1a238cb8a9d9e33b
isabella <isabellaschmitz00@gmail.com>
2023-04-11 14:49:32 -0300

    che

--- src/screens/RMGameScreen.jsx
+++ src/screens/RMGameScreen.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { View, Image } from "react-native";
 import { Button, Text } from "react-native-paper";
-// import {styles} from "../utils/style";
+import { styles } from "../utils/styles";
 
 export default function RMGameScreen() {
   const [personagem, setPersonagem] = useState(null);
   const [personagens, setPersonagens] = useState(0);
   const [totalPersonagens, setTotalPersonagens] = useState(0);
+  const [resposta, setResposta] = useState(null);
 
   useEffect(() => {
     retornaTotalPersonagens();
@@ -22,9 +23,7 @@ export default function RMGameScreen() {
       });
   }
   function retornaTotalPersonagens() {
-    fetch(
-      "https://rickandmortyapi.com/api/character/"
-    )
+    fetch("https://rickandmortyapi.com/api/character/")
       .then((response) => response.json())
       .then((json) => {
         setTotalPersonagens(json.info.count);
@@ -34,28 +33,38 @@ export default function RMGameScreen() {
     return Math.floor(Math.random() * totalPersonagens) + 1;
   }
 
-  function checkIfPersonagemIsAlive() {
-    if (personagem.status === "Alive") {
-      return true;
-    } else {
-      return false;
-    }
+function checkIfPersonagemEstaVivo() {
+  if (personagem.status === "Alive") {
+    setResposta(true);
+  } else {
+    setResposta(false);
   }
+}
 
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <Text>Este personagem está vivo?</Text>
       <Image
         source={{ uri: personagem?.image }}
-        style={{ width: 200, height: 200 }}
+        style={{ width: 200, height: 200, alignItems: "center" }}
       ></Image>
-      <Text>Personagem: {personagem?.name}</Text>
-      <View>
-        <Button>Sim</Button>
-        <Button>Não</Button>
-      </View>
-
-      <Button onPress={buscaPersonagemAleatorio}> Buscar Personagem</Button>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "center",
+          marginVertical: 20,
+        }}
+      >
+        Personagem: {personagem?.name}
+      </Text>
+      <Text>O personagem está vivo?</Text>
+      {resposta !== null && <Text>{resposta ? "Sim" : "Não"}</Text>}
+      <Button onPress={() => checkIfPersonagemEstaVivo()} mode="conteined">
+        Checar
+      </Button>
+      <Button onPress={buscaPersonagemAleatorio} mode="contained">
+        Buscar Personagem
+      </Button>
     </View>
   );
 }
